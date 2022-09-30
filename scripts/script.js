f0cbd5792d40504a8324868e5d0ccf9a10209761
Elena <eceravalo@gmail.com>
2022-09-29 23:43:22 -0400

    Website is complete for sympo. removed js message

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,2 @@
-alert('This website is still under construction! Please pardon the mess');
-
-//this is purely for the development stages of this site
-//will remove in later versions
+//alert('This website is still under construction! Please pardon the mess');
+//this is purely for the development stages of this site will remove in later versions
